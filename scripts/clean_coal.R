793fd786a69d990ce2f523b5628dc390f44b20d6
Peter Boshe <peterboshe@gmail.com>
2022-06-16 00:05:58 +0300

    - added cleaning script  - added eda script

---
+++ scripts/clean_coal.R
@@ -0,0 +1,74 @@
+# script for cleaning coal data
+
+# Author: pedro
+# Version: 2022-06-15
+
+# Packages
+library(tidyverse)
+library(janitor)
+library(GGally)
+
+# Parameters
+
+  # input
+file_raw <- here::here("data/coal_data.rds")
+  # output
+file_out <- here::here("data/clean_coal.rds")
+# ============================================================================
+
+# Code
+coal_df <- read_rds(file_raw)
+
+coal_df |>
+  skimr::skim()
+
+# additional info
+# EG.ELC.COAL.ZS - Electricity production from coal sources (% of total)
+# EG.ELC.FOSL.ZS - Electricity production from oil, gas and coal sources (% of total)
+# EG.USE.COMM.FO.ZS - Fossil fuel energy consumption (% of total)
+# NY.GDP.COAL.RT.ZS - Coal rents (% of GDP)
+# NW.NCA.SACO.TO - Natural capital, subsoil assets: coal (constant 2014 US$) - Natural capital includes the valuation of fossil fuel energy (oil, gas, hard and soft coal) and minerals (bauxite, copper, gold, iron ore, lead, nickel, phosphate, silver, tin, and zinc), agricultural land (cropland and pastureland), forests (timber and some nontimber forest products), and protected areas.  Values are measured at market exchange rates in constant 2014 US dollars, using a country-specific GDP deflator.
+
+
+# 270111 -  Coal; anthracite, whether or not pulverised, but not agglomerated
+# 270112 - Coal; bituminous, whether or not pulverised, but not agglomerated
+# 270119 - Coal; (other than anthracite and bituminous), whether or not pulverised but not agglomerated
+
+
+# clean the data
+
+coal_df |>
+  clean_names() |>
+  select(year,
+         reporter_iso,
+         partner_iso,
+         commodity_code,
+         trade_value_usd_imp,
+         trade_value_usd_exp,
+         commodity_fullname_english,
+         ny_gdp_coal_rt_zs,
+         nw_nca_saco_to) |>
+  rename(
+    coal_rent_pct_of_gdp = ny_gdp_coal_rt_zs,
+    natural_capital = nw_nca_saco_to,
+    commodity = commodity_fullname_english
+  ) |>
+  mutate(commodity = case_when(
+    str_detect(commodity_code, "270111") ~ "anthracite",
+    str_detect(commodity_code, "270112") ~ "bituminous",
+    str_detect(commodity_code, "270119") ~ "other"
+  )) |>
+  select(-commodity_code) |> # drop the commodity code
+  arrange(year) |>
+  na.omit() |>  # drop rows with missing variables
+  mutate(trade_id = row_number()) |>
+  select(trade_id,everything()) |>
+  write_rds(file_out)
+
+
+library(GGally)
+clean_coal |>
+  ggpairs(columns = c(2,5,6,7,8,9),
+          mapping = ggplot2::aes(color = commodity),
+          diag = list(alpha = 0.5))
+
